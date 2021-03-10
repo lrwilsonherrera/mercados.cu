d05f3ea40ea3081ef7ab8e3e0ffb37eda0791548
alian8001 <alimaldi.72016@gmail.com>
2021-03-10 16:57:25 -0500

    alian incluidos el login y registro en el menu de las tiendas

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -12,6 +12,8 @@
                 <li><a class="page-scroll" @if($menu == 'mercabal') style="color: #136689"; @endif href="{{ route('mercabal') }}">MERCABAL</a></li>
                 <li><a class="page-scroll " @if($menu == 'bodegas') style="color: #136689"; @endif href="{{ route('bodegas') }}">BODEGAS</a></li>
                 <li><a class="page-scroll" @if($menu == 'ensucasa') style="color: #136689"; @endif href="{{ route('ensucasa') }}">ENSUCASA</a></li>
+                <li><a class="page-scroll" @if ($menu == 'entrar') style="color: #136689";  @endif href="{{ route('login') }}">ENTRAR</a></li>
+                <li><a class="page-scroll" @if ($menu == 'registrarse') style="color: #136689"; @endif href="{{ route('register') }}">REGISTRARSE</a></li>
             @endif   
                 
         </ul>
